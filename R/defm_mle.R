e51114c6e3d5149e80fea94e2c623d8530797697
George G. Vega Yon <g.vegayon@gmail.com>
2025-09-08 19:27:24 +0000

    Adding function for improved tabulation

--- R/defm_mle.R
+++ R/defm_mle.R
@@ -151,6 +151,127 @@ summary_table <- function(object, as_texreg = FALSE, ...) {
 
   }
 
+}
 
+#' Takes a list of a single fitted model and groups the terms into
+#' interpretable groups.
+#' @param fits A fitted model or a list of fitted models.
+#' @param skip_intercept TRUE/FALSE. Whether to skip the intercept terms
+#' from the output.
+#' @return A list with three elements:
+#' * `custom.coef.map`: A named list that maps the original coefficient names
+#' to a cleaner version.
+#' * `reorder.coef`: An integer vector that indicates how to reorder the
+#' coefficients.
+#' * `groups`: A named list that groups the coefficients into interpretable
+#' groups.
+#' @noRd
+group_terms <- function(
+  fits,
+  skip_intercept = FALSE
+  ) {
+
+  # We want to treat all as a list
+  if (!inherits(fits, "list")) {
+    fits <- list(fits)
+  }
+
+  replacement <- c(
+    "Logit intercept\\s+(.+)" = "\\1",
+    "(Motif\\s+)?\\{([^,]+),\\s*([^}]+)\\}" = "{\\2, \\3}",
+    "(Motif\\s+)?\\{([^,]+),\\s*([^}]+)\\}(.+)" = "{\\2, \\3}\\4"
+  )
+
+  vnames <- lapply(fits, coef) |>
+    unname() |>
+    unlist() |>
+    names() |>
+    unique()
+
+  vnames_new <- gsub(
+    x = vnames,
+    replacement = replacement[1],
+    pattern = names(replacement)[1]
+  ) |>
+    gsub(
+      replacement = replacement[2],
+      pattern = names(replacement)[2]
+    )
+
+  # Identifying the order
+  term_patterns <- names(replacement)
+  terms_intercept <- which(grepl(pattern = term_patterns[1], x = vnames))
+  terms_motifs <- which(grepl(pattern = term_patterns[2], x = vnames))
+  terms_transitions <- which(grepl(pattern = term_patterns[3], x = vnames))
+
+  # Removing the motifs from the transitions
+  terms_motifs <- setdiff(terms_motifs, terms_transitions)
+
+  # Reorganizing terms in this sort
+  terms_order <- c(terms_intercept, terms_motifs, terms_transitions)
+
+  terms_groups <- NULL
+  if (length(terms_intercept) > 0) {
+    terms_groups <- c(terms_groups, list(
+      "Logit intercept" = seq_along(terms_intercept)
+    ))
+  }
+
+  nterms <- length(unlist(terms_groups))
+  if (length(terms_motifs) > 0) {
+    terms_groups <- c(terms_groups, list(
+      "Co-ocurrence" = seq_along(terms_motifs) + nterms
+    ))
+  }
+
+  nterms <- length(unlist(terms_groups))
+  if (length(terms_transitions) > 0) {
+    terms_groups <- c(terms_groups, list(
+      "Transition" = seq_along(terms_transitions) + nterms
+    ))
+  }
+
+  coefmap <- structure(
+    vnames_new,
+    names = vnames
+  ) |> as.list()
+
+  if (skip_intercept) {
+    coefmap <- coefmap[!grepl("^Logit intercept nsi_[a-zA-Z_]+$", names(coefmap))]
+  }
+
+  list(
+    custom.coef.map = coefmap,
+    reorder.coef = terms_order,
+    groups = terms_groups,
+  )
+
+}
+
+#' @param fits Either a single or a list of `defm` fit objects.
+#' @param fun Function to be called from the `texreg` package, e.g., `texreg::screenreg`.
+#' @param ... Further arguments passed to `summary_table` (with the exception of `as_texreg`, which is set to `TRUE`).
+#' @param skip_intercept Whether or not to skip the intercept (logit) terms when printing the table
+#' @rdname defm_mle
+#' @returns
+#' An object of class texreg with additional attributes: `custom.coef.map`,
+#' `reorder.coef`, and `groups`.
+#' @export
+texreg_fancy <- function(
+  fits,
+  fun,
+  skip_intercept = FALSE,
+  ...
+) {
+
+  extra_info <- group_terms(fits, skip_intercept = skip_intercept)
+
+  do.call(
+    fun,
+    c(
+      list(lapply(fits, summary_table, as_texreg = TRUE, ...)),
+      group_terms(fits)
+      )
+  )
 
 }
