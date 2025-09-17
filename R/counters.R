6a92517a267ad2a0db4d630e8760c88f295ab9f7
George G. Vega Yon <g.vegayon@gmail.com>
2025-09-17 01:05:29 +0000

    Adding helper function that makes it easier to rename terms

--- R/counters.R
+++ R/counters.R
@@ -29,3 +29,72 @@ as.list.DEFM_counter <- function(x, ...) {
   as_list_defm_counter(x)
 
 }
+
+#' @export
+#' @rdname get_counters
+#' @param model A [DEFM] model object.
+#' @returns
+#' - The function `set_counters_names()` returns the counters invisibly.
+set_counters_names <- function(x, ...) UseMethod("set_counters_names")
+
+#' @export
+#' @rdname get_counters
+set_counters_names.DEFM <- function(x, ...) {
+  set_counters_names(get_counters(x), ...)
+  invisible(x)
+}
+
+#' @export
+#' @rdname get_counters
+set_counters_names.DEFM_counters <- function(
+  x, ...
+) {
+
+  dots <- list(...)
+
+  args_positions <- names(dots)
+  tmp_test <- which(args_positions == "")
+  if (length(tmp_test) > 0L) {
+    stop(
+       "Arguments passed via `...` should be named. ",
+       "The following positions were empty (\"\"): ",
+       paste(tmp_test, collapse = ", ")
+      )
+  }
+
+  # Figuring out the positions (can be turned integers)
+  args_positions <- suppressWarnings({
+    as.integer(args_positions)
+  })
+
+  tmp_test <- which(is.na(args_positions))
+  if (length(tmp_test)) {
+    stop(
+      "Arguments passed via `...` should be named with integers ",
+      "indicating the position of the counter to modify. ",
+      "The following positions were not integers: ",
+      paste(tmp_test, collapse = ", ")
+    )
+  }
+
+
+  n_counters <- length(x)
+  if (any(args_positions < 0L) || any(args_positions >= n_counters)) {
+    stop(
+      "Arguments passed via `...` should be named with integers ",
+      "indicating the position of the counter to modify. ",
+      "The valid range is from 0 to ", n_counters - 1L, "."
+    )
+  }
+
+  # Getting the counters
+  Map(\(pos, new_name) {
+    set_counter_info(
+      x[pos],
+      new_name = new_name
+    )
+  }, pos = args_positions, new_name = unlist(dots))
+
+  invisible(x)
+
+}
